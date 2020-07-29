5605f20f1f3ae2133d614c9184c8d862e6633605
scotty35 <68961718+scotty35@users.noreply.github.com>
2020-07-29 11:34:39 -0600

    Add files via upload

---
+++ slide-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/bin/feh --quiet --preload --auto-zoom --full-screen --reload 60 -Y --slideshow-delay 15.0 /home/pi/Desktop/slideshow/images &
+
+
